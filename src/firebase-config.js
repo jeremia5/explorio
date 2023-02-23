c4a3efdedae720277a3fd349f3d511e07899ad4b
jeremih <jeremih@stud.ntnu.no>
2023-02-23 23:54:10 +0100

    Added new database

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,12 +8,12 @@ import {getAuth, GoogleAuthProvider} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDDf7guAq7ennt0jCNjklA-7lNsASWD8o0",
-  authDomain: "explorioverthree.firebaseapp.com",
-  projectId: "explorioverthree",
-  storageBucket: "explorioverthree.appspot.com",
-  messagingSenderId: "168321753262",
-  appId: "1:168321753262:web:247a2909f4c881d7764d3f"
+  apiKey: "AIzaSyAKSnYcnkt0WFHJxpqVfS-42_DhhNUqndo",
+  authDomain: "explorioverfour.firebaseapp.com",
+  projectId: "explorioverfour",
+  storageBucket: "explorioverfour.appspot.com",
+  messagingSenderId: "558859548034",
+  appId: "1:558859548034:web:9fea629303c7da3ae20b9b"
 };
 
 // Initialize Firebase
